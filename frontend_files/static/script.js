f8c7da38879431a34c756a7b725efb3d1c008606
Konstantinos Larintzakis <konstantinos.larintzakis@tum.de>
2023-06-11 13:28:13 +0200

    deleted content of script.js

--- frontend_files/static/script.js
+++ frontend_files/static/script.js
@@ -1,20 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('upload-form');
-    const submitBtn = document.getElementById('submit-btn');
-    const loadingDiv = document.getElementById('loading-div');
-    const resultDiv = document.getElementById('result-div');
-    const resultText = document.getElementById('result-text');
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        // Disable the submit button and show loading screen
-        submitBtn.disabled = true;
-        loadingDiv.style.display = 'block';
-
-        // Create a FormData object to send the file via AJAX
-        const formData = new FormData(form);
-        const xhr = new
 
 
 
