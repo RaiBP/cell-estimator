953e45703e450c9f9b4a14d9300bae9b2ad11d30
Leonardo Oliveira <leonardo.fernandes.oliv@gmail.com>
2023-06-30 12:03:59 +0200

    Add first version of polygon manager component

--- app/frontend/my-app2/src/App.js
+++ app/frontend/my-app2/src/App.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Stage, Layer, Line, Circle, Image } from 'react-konva';
+import { Stage, Layer, Group, Line, Circle, Image } from 'react-konva';
 
 const AnnotationArea = () => {
 
   const [image, setImage] = useState(null);
-  const [lines, setLines] = useState([]);
+
+  // Polygon management
+  const [polygons, setPolygons] = useState({});
+  const [polygonCounter, setPolygonCounter] = useState(0);
+  const [currentPolygon, setCurrentPolygon] = useState([]);
+  const currentPolygonRef = React.useRef(currentPolygon);
+
   const [previewLine, setPreviewLine] = useState(null);
-  const linesRef = React.useRef(lines);
   const isDrawing = React.useRef(false);
 
   // Hook for keeping track of lines
   useEffect(() => {
-    linesRef.current = lines;
-  }, [lines]);
+    currentPolygonRef.current = currentPolygon;
+  }, [currentPolygon]);
 
   // Hook for registering keydown events -- happens only when component is mounted
   useEffect(() => {
@@ -26,7 +31,7 @@ const AnnotationArea = () => {
     // Handling keydown events -- registering callback
     const handleKeyDown = (event) => {
       if (event.key === 'r') {
-        reset();
+        resetCurrentPolygon();
       }
       else if (event.key === 'z' && event.ctrlKey) {
         undo();
@@ -48,32 +53,31 @@ const AnnotationArea = () => {
   const handleMouseDown = (e) => {
     isDrawing.current = true;
     if (previewLine) {
-      setLines([...lines, previewLine]);
+      setCurrentPolygon([...currentPolygon, previewLine]);
       setPreviewLine(null);
     } else {
       const pos = e.target.getStage().getPointerPosition();
-      setLines([...lines, { points: [pos.x, pos.y, pos.x, pos.y] }]);
+      setCurrentPolygon([...currentPolygon, { points: [pos.x, pos.y, pos.x, pos.y] }]);
     };
-    console.log(lines);
   }
 
   const handleMouseMove = (e) => {
     const stage = e.target.getStage();
     const point = stage.getPointerPosition();
 
-    if (!isDrawing.current || lines.length == 0) {
+    if (!isDrawing.current || currentPolygon.length == 0) {
       return;
     }
 
-    const lastLineEnd = lines[lines.length - 1].points.slice(2);
+    const lastLineEnd = currentPolygon[currentPolygon.length - 1].points.slice(2);
 
     // Computing current distance to first point
-    const firstPoint = lines[0].points;
+    const firstPoint = currentPolygon[0].points;
     const dx = firstPoint[0] - point.x;
     const dy = firstPoint[1] - point.y;
     const distance = Math.sqrt(dx * dx + dy * dy);
     if (distance < 15) {
-      const firstPoint = lines[0].points;
+      const firstPoint = currentPolygon[0].points;
       setPreviewLine({ points: [...lastLineEnd, ...firstPoint] });
     } else {
       setPreviewLine({ points: [...lastLineEnd, point.x, point.y] });
@@ -86,76 +90,105 @@ const AnnotationArea = () => {
   };
 
   function stopDrawing() {
+    saveMask();
     isDrawing.current = false;
     setPreviewLine(null);
   }
 
   function saveMask() {
-    console.log(linesRef.current);
+
+    if (currentPolygonRef.current.length <= 2) {
+      resetCurrentPolygon();
+      return;
+    }
+
+    setPolygonCounter((prevCount) => {
+      setPolygons((prevPolygons) => {
+        return { ...prevPolygons, [prevCount]: currentPolygonRef.current.slice(0) };
+      });
+      return prevCount + 1;
+    });
+    setCurrentPolygon([]);
+  }
+
+  function resetCurrentPolygon() {
+    setCurrentPolygon([]);
+    setPreviewLine(null);
   }
 
   function reset() {
-    setLines([]);
+    setPolygons({});
+    setCurrentPolygon([]);
+    setPolygonCounter(0);
     setPreviewLine(null);
   }
 
   function undo() {
     setPreviewLine(null);
-    setLines((lines) => lines.slice(0, -1));
+    setCurrentPolygon((lines) => lines.slice(0, -1));
   }
 
+  return (
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
+      <Layer>
+        {image && (
+          <Image
+            image={image}
+            x={0}
+            y={0}
+            width={window.innerWidth}
+            height={window.innerHeight}
+          />
+        )}
+        {Object.entries(polygons).map(([polygonId, polygon]) => {
+          return <Polygon id={polygonId} lines={polygon} />
+        })}
+        <Polygon lines={currentPolygon} />
+        {previewLine && (
+          <Line
+            points={previewLine.points}
+            stroke="#df4b26"
+            strokeWidth={5}
+            tension={0.5}
+            lineCap="round"
+            lineJoin="round"
+          />
+        )}
+      </Layer>
+    </Stage>
+  );
+};
+
+function Polygon({ id, lines }) {
 
   return (
-    <div>
-      <Stage
-        width={window.innerWidth}
-        height={window.innerHeight}
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
-      >
-        <Layer>
-          {image && (
-            <Image
-              image={image}
-              x={0}
-              y={0}
-              width={window.innerWidth}
-              height={window.innerHeight}
+    <Group>
+      {lines.map((line, i) => (
+        <>
+          <Line
+            key={i}
+            points={line.points}
+            stroke="#df4b26"
+            strokeWidth={2}
+            tension={0.2}
+          />
+          {
+            <Circle
+              x={line.points[2]}
+              y={line.points[3]}
+              radius={5}
+              fill="black"
             />
-          )}
-          {lines.map((line, i) => (
-            <>
-              <Line
-                key={i}
-                points={line.points}
-                stroke="#df4b26"
-                strokeWidth={2}
-                tension={0.2}
-              />
-              {
-                <Circle
-                  x={line.points[2]}
-                  y={line.points[3]}
-                  radius={5}
-                  fill="black"
-                />
-              }
-            </>
-          ))}
-          {previewLine && (
-            <Line
-              points={previewLine.points}
-              stroke="#df4b26"
-              strokeWidth={5}
-              tension={0.5}
-              lineCap="round"
-              lineJoin="round"
-            />
-          )}
-        </Layer>
-      </Stage>
-    </div>
+          }
+        </>
+      ))}
+    </Group >
   );
 };
 
@@ -166,13 +199,6 @@ class App extends React.Component {
       counter: 0,
       image: null,
     }
-  }
-
-  incrementCounter() {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-    console.log(this.state.counter)
   }
 
   render() {
