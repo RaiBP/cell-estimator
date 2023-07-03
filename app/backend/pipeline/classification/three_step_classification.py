8e5e3d28413c564fcc826d0af4f7e8435ed5d2da
RaiBP <raimundobecerraparra@gmail.com>
2023-07-03 18:26:06 +0200

    Moved the entire inference pipeline to app/backend/pipeline. I cleaned the code a bit, each class is now self contained. I changed the name of the class Pipeline to Inference, since the name Pipeline could have conflicts with the Pipeline class from sklearn, and also it emphasizes that all the class does is inferenece. Actually, I removed all the methods that had to do with training from all the classes. So for inference, all we do is load pre-trained models. I think we should focus on getting the inference working cleanly for now, and when it's time to implement active learning, we can start adding new methods for training models (in a new class, for example). I will upload the .pkl files for the ThreeStepClassification model ASAP. For now, just use the OneStepClassification models. I haven't touched SAM, and ThresholdSegmentation still needs some work to get the masks output right.

---
+++ app/backend/pipeline/classification/three_step_classification.py
@@ -0,0 +1,89 @@
+import pandas as pd
+
+from classification import Classification
+
+
+class ThreeStepClassifier(Classification):
+    def __init__(self, oof_model_filename=None, agg_model_filename=None, cell_model_filename=None):
+        super().__init__()
+        
+        if oof_model_filename is None:
+            self.oof_model_filename = "tsc_oof_model.pkl"
+        else:
+            self.oof_model_filename = oof_model_filename
+        if agg_model_filename is None:
+            self.agg_model_filename = "tsc_agg_model.pkl"
+        else:
+            self.agg_model_filename = agg_model_filename
+        if cell_model_filename is None:
+            self.cell_model_filename = "tsc_cell_model.pkl"
+        else:
+            self.cell_model_filename = cell_model_filename
+
+        self.oof_model = self._load_model(self.models_folder, self.oof_model_filename)
+        self.agg_model = self._load_model(self.models_folder, self.agg_model_filename)     
+        self.cell_model = self._load_model(self.models_folder, self.cell_model_filename)     
+
+
+    def _get_predictions(self, features):
+        df = self._predict(features)
+        return df[self.labels_column_name].tolist()
+
+    
+    def _get_probabilities(self, features):
+        oof_proba, agg_proba, cell_proba = self._predict_proba(features)
+        return {'oof_proba': oof_proba, 'agg_proba': agg_proba, 'cell_proba': cell_proba} 
+
+
+    def _predict_proba(self, df):
+        return self._predict_proba_oof(df), self._predict_proba_agg(df), self._predict_proba_cell(df)
+
+
+    def _predict_proba_oof(self, df):
+        return self.oof_model.predict_proba(df)
+
+
+    def _predict_proba_agg(self, df):
+        return self.agg_model.predict_proba(df)
+
+
+    def _predict_proba_cell(self, df):
+        return self.cell_model.predict_proba(df)
+
+
+    def _predict(self, df):
+        df_list = []
+
+        oof_labels = self._oof_predict(df)
+        df_in_focus = df[oof_labels == 0].copy()
+        df_oof = df[oof_labels == 1].copy()
+        df_oof[self.labels_column_name] = self.out_of_focus_label
+
+        df_list.append(df_oof)
+        
+        agg_labels = self._agg_predict(df_in_focus)
+        df_single_cell = df_in_focus[agg_labels == 0].copy()
+        df_agg = df_in_focus[agg_labels == 1].copy()
+        df_agg[self.labels_column_name] = self.aggregate_label
+        df_list.append(df_agg)
+
+        cell_labels = self._cell_predict(df_single_cell)
+
+        df_single_cell.loc[:, self.labels_column_name] = cell_labels
+        df_list.append(df_single_cell)
+
+        return pd.concat(df_list).reindex(df.index)
+
+
+    def _oof_predict(self, df):
+        return self.oof_model.predict(df)
+
+
+    def _agg_predict(self, df):
+        return self.agg_model.predict(df)
+
+
+    def _cell_predict(self, df): 
+        return self.cell_model.predict(df)
+
+
---
+++ app/backend/pipeline/segmentation/sam_segmentation.py
@@ -0,0 +1,12 @@
+from segmentation import Segmentation
+
+class SAMSegmentation(Segmentation):
+    def __init__(self, batch, parameters):
+        super().__init__(batch)
+        self.parameters = parameters
+
+    def segment(self):
+        # Implement SAM-based segmentation here
+        # Use self.img and self.parameters to perform the segmentation
+        # Return the segmented image
+        pass
