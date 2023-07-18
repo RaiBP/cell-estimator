b1bc590ee1bbdd2ee4593eb602938a0d2ddccaaf
Ivan Nikolovski <ivan.nikolovski@tum.de>
2023-07-18 14:01:43 +0200

    Fixed explain menu size

--- app/frontend/my-app2/src/components/ExplainMenu/ExplainMenu.js
+++ app/frontend/my-app2/src/components/ExplainMenu/ExplainMenu.js
@@ -1,4 +1,3 @@
-
 import Button from '../Button/Button'
 import './ExplainMenu.css'
 import '../Menu/Menu.css'
@@ -9,14 +8,14 @@ function ExplainMenu({ handleOptionClick, selectorOptions, handleXAxisFeatureCha
     return(
         <div className="explain-menu" style={{ left: '${x}px', top: '${y}px'}}>
             <Button className="explain-menu-button" onClick={() => handleOptionClick('close')}>Close</Button>
-            <FeatureSelector handleChange={handleXAxisFeatureChange} caption="Choose a feature for the X-axis" options={selectorOptions} />
-            <FeatureSelector handleChange={handleYAxisFeatureChange} caption="Choose a feature for the Y-axis" options={selectorOptions} />
+            <FeatureSelector className="explain-menu-selector-container" handleChange={handleXAxisFeatureChange} caption="Choose a feature for the X-axis" options={selectorOptions} />
+            <FeatureSelector className="explain-menu-selector-container" handleChange={handleYAxisFeatureChange} caption="Choose a feature for the Y-axis" options={selectorOptions} />
         </div>) 
 }
 
 function FeatureSelector({ handleChange, caption, options }) {
   return (
-    <div className="selector-container">
+    <div className="explain-menu-selector-container">
       <label htmlFor='feature-selector' className="selector-label"> {caption} </label>
       <select id='dataset-selector' className="selector" onChange={handleChange}>
         {options.map((option, index) => (
